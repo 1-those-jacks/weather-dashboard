487f255dbf38f2808788b5783d64da7be1cdf6f7
1-those-jacks <85271008+1-those-jacks@users.noreply.github.com>
2022-11-03 01:35:01 -0500

    Got fetch to respond with data based on input to API.

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,12 @@ var searchBtn = document.querySelector("#search-button");
 var searchHistory = document.querySelector("#search-history");
 var fiveDayForecast = document.querySelector("#five-day");
 
-var requestURL = "https://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=b2396c411f7ee6c8e084346e085d556d";
 
 function searchByCity(event){
+	
 	// prevent default/stop propagation or something
 	event.preventDefault();
-	console.log(event);
+	// console.log(event);
 	// input has data entered into it
 	var searchResponse = searchInput.value;
 	console.log(searchResponse);
@@ -17,17 +17,47 @@ function searchByCity(event){
 	// information is saved in local storage
 	localStorage.setItem("search", JSON.stringify(searchResponse));
 	// search.text is added to five-day
-	var cityName = document.querySelector("span");
+	var cityName = document.querySelector("h3");
 	cityName.textContent = searchResponse;
 	// search.text is added to history - as a button
 	var addCity = document.createElement("button");
 	addCity.textContent = searchResponse;
 	searchHistory.appendChild(addCity);
+// add UL and then make button LI
 	// search.weather is added to current-weather
-	// getApi(returnDates){
+			// target history button
 
-	// };
-	for (var i = 0; i < 5; i++){
+		// objectArray.city.name
+	function	getApi() {
+		var requestURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + "" + searchResponse + "" + "&appid=b2396c411f7ee6c8e084346e085d556d";
+		fetch(requestURL)
+			.then(function (response) {
+				if (response.ok) {
+					response.json().then(function (data) {
+						console.log(data.city);
+						console.log(data.list);
+						for (var i = 0; i < data.list.length; i++) {
+							console.log(data.list[i]);
+						}
+					});
+				} else {
+					alert("ERROR: " + response.statusText);
+				}
+			})
+			.catch(function (error) {
+				alert(error);
+			});
+		};
+						// function displayRepos(city, searchTerm){
+		
+		// 				console.log(data.list[i]);
+		// 				return;
+		// 			};
+		// // 			}
+		// 			)
+		// });
+
+		for (var i = 0; i < 5; i++){
 		// add date - dateData
 		var addDate = document.createElement("h4");
 		addDate.textContent = "Date: ";
@@ -54,22 +84,8 @@ function searchByCity(event){
 		fiveDayForecast.appendChild(addWindSpeed);
 	}
 	// search.weather is added to five-day
+	getApi();
 }
 
-function searchByHistory(){
-	// target history button
-	// on click event run search by city
-}
-
-function	getApi() {
-	fetch(requestURL)
-		.then(function(response) {
-			return response.json();
-		})
-		.then(function (data) {
-			console.log(data);
-		})
-}
-
-searchBtn.addEventListener("click", searchByCity);
-
+			// on click event run search by city
+searchBtn.addEventListener("click", searchByCity)
